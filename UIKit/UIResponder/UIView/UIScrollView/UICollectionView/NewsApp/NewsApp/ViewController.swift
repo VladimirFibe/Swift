cf05f1d99f5371ebd1573090303bd2a914770aea
Vladimir Fibe <motiw@icloud.com>
2023-09-21 10:12:11 +0600

    ViewController modify

--- UIKit/UIResponder/UIView/UIScrollView/UICollectionView/NewsApp/NewsApp/ViewController.swift
+++ UIKit/UIResponder/UIView/UIScrollView/UICollectionView/NewsApp/NewsApp/ViewController.swift
@@ -1,19 +1,13 @@
-//
-//  ViewController.swift
-//  NewsApp
-//
-//  Created by Vladimir Fibe on 21.09.2023.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        navigationItem.title = "News"
     }
-
-
 }
+
+
 
